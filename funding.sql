8c09759a97a7b7c5efab4fc59cf26dcb384e79f0
MelissaBoateng <67985151+MelissaBoateng@users.noreply.github.com>
2021-04-19 05:08:13 +0300

    database schema

---
+++ funding.sql
@@ -0,0 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2021 at 02:18 AM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `funding`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE `country` (
+  `country_id` int(11) NOT NULL,
+  `country_name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fundee`
+--
+
+CREATE TABLE `fundee` (
+  `fundee_id` int(11) NOT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `Fname` varchar(250) DEFAULT NULL,
+  `lname` varchar(250) DEFAULT NULL,
+  `password` varchar(250) NOT NULL,
+  `country_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `industry`
+--
+
+CREATE TABLE `industry` (
+  `industry_id` int(11) NOT NULL,
+  `industry_type` varchar(250) DEFAULT NULL,
+  `date` date NOT NULL,
+  `status` varchar(30) DEFAULT NULL,
+  `net_balance` int(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `investor`
+--
+
+CREATE TABLE `investor` (
+  `investor_id` int(11) NOT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `Fname` varchar(250) DEFAULT NULL,
+  `lname` varchar(250) DEFAULT NULL,
+  `password` varchar(250) NOT NULL,
+  `country_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `project_id` int(11) NOT NULL,
+  `project_name` varchar(250) DEFAULT NULL,
+  `industry_type` varchar(255) DEFAULT NULL,
+  `date` date NOT NULL,
+  `status` varchar(250) DEFAULT NULL,
+  `Net_balance` int(40) DEFAULT NULL,
+  `country_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_tracker`
+--
+
+CREATE TABLE `project_tracker` (
+  `project_id` int(11) NOT NULL,
+  `fundee_id` int(11) NOT NULL,
+  `investor_id` int(11) NOT NULL,
+  `industry_type` varchar(255) DEFAULT NULL,
+  `project_name` varchar(250) NOT NULL,
+  `date` date NOT NULL,
+  `status` varchar(40) DEFAULT NULL,
+  `Net_balance` int(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`country_id`);
+
+--
+-- Indexes for table `fundee`
+--
+ALTER TABLE `fundee`
+  ADD PRIMARY KEY (`fundee_id`);
+
+--
+-- Indexes for table `industry`
+--
+ALTER TABLE `industry`
+  ADD PRIMARY KEY (`industry_id`);
+
+--
+-- Indexes for table `investor`
+--
+ALTER TABLE `investor`
+  ADD PRIMARY KEY (`investor_id`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`project_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
